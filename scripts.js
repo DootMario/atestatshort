49ea04e365b6cdb81bc947ed4af4efde9dd3227b
DootMario <rautumihai.ionut@gmail.com>
2025-03-03 22:14:11 +0200

    wild did homepage fixed(?) links to social and what not maybe take a look at the spotify embed later css shenaniganery need be il actually learn js to do this stuff properly omw to the history and bibliografy page next and then just make a gallery quick and its ggs

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-import * as boot from "external/bootstrap-5.3.3-dist/js"
+// import * as boot from "external/bootstrap-5.3.3-dist/js"
+
+const homeLinks = document.getElementById('homeLinks');
+
+function worh() {
+
+    if (window.innerWidth<= 800) {
+        homeLinks.classList.remove('btn-group')
+        homeLinks.classList.add('btn-group-vertical')
+    }
+
+}
