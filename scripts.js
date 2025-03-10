ae0139c9284cd0d9c859560d20a4b70d6849f39d
DootMario <rautumihai.ionut@gmail.com>
2025-03-10 16:11:06 +0200

    working on js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,7 @@
 // import * as boot from "external/bootstrap-5.3.3-dist/js"
 
-const homeLinks = document.getElementById('homeLinks');
-
 function worh() {
-
+    const homeLinks = document.getElementById('homeLinks');
     if (window.innerWidth<= 800) {
         homeLinks.classList.remove('btn-group')
         homeLinks.classList.add('btn-group-vertical')
